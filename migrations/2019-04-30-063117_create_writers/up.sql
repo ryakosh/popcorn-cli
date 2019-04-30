bd06b896d4b72ae19306f7f0ca1625d759318d78
ryakosh <ryakosh@gmail.com>
2019-04-30 11:20:45 +0430

    Setup diesel

---
+++ migrations/2019-04-30-063117_create_writers/up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS writers(
+  writer_id SERIAL PRIMARY KEY,
+  first_name VARCHAR NOT NULL,
+  last_name VARCHAR NOT NULL,
+  gender VARCHAR(1) NOT NULL
+);
---
+++ migrations/2019-04-30-063447_create_movies/down.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS movies_writers;
+DROP TABLE IF EXISTS movies_directors;
+DROP TABLE IF EXISTS movies_artists;
+
+DROP INDEX IF EXISTS idx_genres;
+DROP INDEX IF EXISTS idx_languages;
