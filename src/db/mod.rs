d5a8d5d8f1d219fa87c8b56679afc5a4e2966a64
ryakosh <ryakosh@gmail.com>
2019-05-02 17:02:18 +0430

    Add cast_add

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -4,23 +4,24 @@ pub mod models;
 use crate::diesel;
 use crate::diesel::prelude::*;
 use crate::types;
+use crate::types::candidates;
 use std::env;
 
 fn connect(conn_str: &str) -> PgConnection {
     PgConnection::establish(conn_str).expect(&format!("Error connecting to: {}", conn_str))
 }
 
-pub fn cast_query(cast: types::CastJob , term: &str) -> types::Casts {
+pub fn cast_query(job: types::CastJob , term: &str) -> types::Casts {
     let conn = connect(&env::var("DATABASE_URL")
         .expect("Can't find DATABASE_URL environment variable"));
     let query = format!(
         include_str!("raw/cast_query.sql"),
-        cast=cast,
+        job=job,
         term=term,
     );
     let query_result = diesel::sql_query(query);
 
-    match cast {
+    match job {
         types::CastJob::Writer => 
             types::Casts::Writers(query_result
                 .load::<models::Writer>(&conn)
@@ -34,4 +35,17 @@ pub fn cast_query(cast: types::CastJob , term: &str) -> types::Casts {
                 .load::<models::Artist>(&conn)
                 .expect("Error executing query")),
     }
+}
+
+pub fn cast_add(candidate: &candidates::CastCandidate) {
+    let conn = connect(&env::var("DATABASE_URL")
+        .expect("Can't find DATABASE_URL environment variable"));
+    
+    diesel::sql_query(format!(
+        include_str!("raw/cast_add.sql"),
+        job=candidate.job(),
+        first_name=candidate.first_name(),
+        last_name=candidate.last_name(),
+        gender=candidate.gender(),
+    )).execute(&conn).expect("Error executing query");
 }
