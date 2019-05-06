2b1960e94de90faf544eba89d35a3c22fd6deec9
ryakosh <ryakosh@gmail.com>
2019-05-06 22:10:05 +0430

    Add writers_query, directors_query and artists_query

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -12,30 +12,37 @@ fn connect(conn_str: &str) -> PgConnection {
     PgConnection::establish(conn_str).expect(&format!("Error connecting to: {}", conn_str))
 }
 
-pub fn cast_query(job: types::CastJob , term: &str) -> types::Casts {
+pub fn writers_query(term: &str) -> Vec<models::Writer> {
     let conn = connect(&env::var("DATABASE_URL")
         .expect("Can't find DATABASE_URL environment variable"));
-    let query = format!(
-        include_str!("raw/cast_query.sql"),
-        job=job,
-        term=term,
-    );
-    let query_result = diesel::sql_query(query);
 
-    match job {
-        types::CastJob::Writer => 
-            types::Casts::Writers(query_result
-                .load::<models::Writer>(&conn)
-                .expect("Error executing query")),
-        types::CastJob::Director => 
-            types::Casts::Directors(query_result
-                .load::<models::Director>(&conn)
-                .expect("Error executing query")),
-        types::CastJob::Artist => 
-            types::Casts::Artists(query_result
-                .load::<models::Artist>(&conn)
-                .expect("Error executing query")),
-    }
+    diesel::sql_query(format!(
+        include_str!("raw/cast_query.sql"),
+        job=types::CastJob::Writer,
+        term=term,
+    )).load::<models::Writer>(&conn).expect("Error executing query")
+}
+
+pub fn directors_query(term: &str) -> Vec<models::Director> {
+    let conn = connect(&env::var("DATABASE_URL")
+        .expect("Can't find DATABASE_URL environment variable"));
+
+    diesel::sql_query(format!(
+        include_str!("raw/cast_query.sql"),
+        job=types::CastJob::Writer,
+        term=term,
+    )).load::<models::Director>(&conn).expect("Error executing query")
+}
+
+pub fn artists_query(term: &str) -> Vec<models::Artist> {
+    let conn = connect(&env::var("DATABASE_URL")
+        .expect("Can't find DATABASE_URL environment variable"));
+
+    diesel::sql_query(format!(
+        include_str!("raw/cast_query.sql"),
+        job=types::CastJob::Writer,
+        term=term,
+    )).load::<models::Artist>(&conn).expect("Error executing query")
 }
 
 pub fn cast_add(candidate: &candidates::CastCandidate) {
